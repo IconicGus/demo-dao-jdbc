1d0f7bb15d022e49633c44cfad96ebc8c5a23a1d
Gustavo Marcal <eletronica1507@gmail.com>
2025-01-03 16:57:26 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,16 +3,20 @@ package application;
 import db.DB;
 import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(3000.0, new Date(), obj, "bob@gmail.com", 21, "Bob");
+
+        System.out.println(seller);
 
     }
 }
